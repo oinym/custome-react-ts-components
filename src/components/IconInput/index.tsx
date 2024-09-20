0b278abb3754790acd537085afb1baec8072271e
Ofir Dayan <ofird79@gmail.com>
2024-09-21 01:07:22 +0300

    init

--- src/components/IconInput/index.tsx
+++ src/components/IconInput/index.tsx
@@ -1,17 +1,14 @@
 import "./styles.css";
 
-
 interface Props {
     class?: string;
     icon: string;// in html code for example: % would be &#37;
     InputProps: any
-
 }
 
 export default function IconInput(props: Props) {
     const baseClass = "input-container";
     const combinedClass = `${baseClass} ${props.class || ''}`.trim();
-
     return (
         <div className={combinedClass} >
             <input {...props.InputProps} />
